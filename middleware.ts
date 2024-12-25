b38d535fa7e623bc0919c34b0aa4333995ae38a5
kanha-9770 <app3.nessco@gmail.com>
2024-12-25 16:36:21 +0530

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,23 @@
-import { geolocation } from '@vercel/functions'
-import { type NextRequest, NextResponse } from "next/server";
-import countries from './lib/countries.json'
+import { geolocation } from '@vercel/functions';
+import { type NextRequest, NextResponse } from 'next/server';
 
 // Run on all routes
 export const config = {
   matcher: '/:path*',
-}
+};
 
 export async function middleware(req: NextRequest) {
-  const geo = geolocation(req)
-  const country = geo.country || 'US'
-  const city = geo.city || 'San Francisco'
-  const region = geo.region || 'CA'
+  // Get geolocation information
+  const geo = geolocation(req);
+  const country = geo.country || 'US'; // Default to 'US' if country is undefined
 
-  const countryInfo = countries.find((x) => x.cca2 === country)
-
-  if (!countryInfo) {
-    return NextResponse.json({ error: 'Country not found' }, { status: 404 })
-  }
-
-  const currencyCode = Object.keys(countryInfo.currencies)[0]
-  const currency = countryInfo.currencies[currencyCode]
-  const languages = Object.values(countryInfo.languages).join(', ')
-
-  const geoData = {
-    country,
-    city,
-    region,
-    currencyCode,
-    currencySymbol: currency.symbol,
-    currencyName: currency.name,
-    languages,
-  }
+  // Return the country as JSON
+  const response = NextResponse.json({ country });
 
   // Set CORS headers
-  const response = NextResponse.json(geoData)
-  response.headers.set('Access-Control-Allow-Origin', '*')
-  response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS')
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-  return response
+  return response;
 }
-
